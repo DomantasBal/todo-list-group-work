2fd72f96e276d9cfb0178d550090f7444e4f6dec
AurimasT <qzmp1793@gmail.com>
2023-03-27 19:10:11 +0300

    login/reg changed

--- Login/login.js
+++ Login/login.js
@@ -1,14 +1,15 @@
-function check(){
-    var storedName = localStorage.getItem('name');
-    var storedPw = localStorage.getItem('pw');
+function check() {
+  var storedName = localStorage.getItem("name");
+  var storedPw = localStorage.getItem("pw");
 
-    var userName = document.getElementById('userName');
-    var userPw = document.getElementById('userPw');
-    var userRemember = document.getElementById("rememberMe");
+  var userName = document.getElementById("userName");
+  var userPw = document.getElementById("userPw");
+  var userRemember = document.getElementById("rememberMe");
 
-    if(userName.value == storedName && userPw.value == storedPw){
-        alert('You are logged in.');
-    }else{
-        alert('Error on login');
-    }
+  if (userName.value == storedName && userPw.value == storedPw) {
+    location.href = "/index.html";
+    alert("You have successfully logged in!");
+  } else {
+    alert("Error on login, try again :(");
+  }
 }
