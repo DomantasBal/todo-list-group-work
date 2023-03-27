f0b763e94866331587bc29624055dd3d2e0274a2
irminaj <irminaj@github.com>
2023-03-27 18:59:14 +0300

    add function to mark a task as favourite

--- favourites/favourites.js
+++ favourites/favourites.js
@@ -7,3 +7,9 @@ const userLogin = () => {
   const loggedUser = users.filter((user) => user.username === username.value && user.password === password.value);
   localStorage.setItem("loggedUser", JSON.stringify(loggedUser));
 };
+
+// Add to-do task to favourites
+const addToFavourites2 = (id) => {
+  todos[id].favourite = true;
+  localStorage.setItem("todo-list", JSON.stringify(todos));
+};
