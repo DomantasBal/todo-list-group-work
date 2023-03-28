4c8adc129a6b683fd073c5a0b54fa786cd067c8b
AurimasT <qzmp1793@gmail.com>
2023-03-28 20:45:00 +0300

    remove ALL

--- deleteFunction/delete.js
+++ deleteFunction/delete.js
@@ -1,25 +1,10 @@
-const taskInput = document.querySelector(".task-input input"),
-  filters = document.querySelectorAll(".filters span"),
-  clearAll = document.querySelector(".clear-btn"),
-  taskBox = document.querySelector(".task-box");
+const clearAll = document.querySelector("#clearAll-btn");
+let list = JSON.parse(localStorage.getItem("todo-list"));
 
-
-let editId,
-    isEditTask = false,
-    todos = JSON.parse(localStorage.getItem("todo-list"));
-
-function deleteTask(deleteId, filter) {
-    isEditTask = false;
-    todos.splice(deleteId, 1);
-    localStorage.setItem("todo-list", JSON.stringify(todos));
-    showTodo(filter);
-  }
-  
 // ! If we have clearAll button !
 
-  clearAll.addEventListener("click", () => {
-    isEditTask = false;
-    todos.splice(0, todos.length);
-    localStorage.setItem("todo-list", JSON.stringify(todos));
-    showTodo();
-  });
+clearAll.addEventListener("click", () => {
+  isEditTask = false;
+  list.splice(0, list.length);
+  localStorage.setItem("todo-list", JSON.stringify(list));
+});
