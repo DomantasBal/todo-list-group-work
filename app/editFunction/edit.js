7c18441f20bafd8646caecf49e6b6f3b584c300c
Domantas <92823413+DomantasBal@users.noreply.github.com>
2023-03-28 16:36:09 +0300

    added minor edit menu styles and visuals. Linked js files for new task generation functionality. Animation on task menu will be improved

--- app/editFunction/edit.js
+++ app/editFunction/edit.js
@@ -1,6 +1,36 @@
-const taskMenu = document.querySelector(".task-menu");
+let counter = 0;
 
-function showEditMenu() {
-  taskMenu.classList.toggle("hide");
-  console.log("hide");
+function showEditMenu(selected) {
+  counter++;
+  counter % 2 == 1 ? openEditMenu(selected) : closeEditMenu(selected);
+}
+
+// OPENS EDIT MENU - SHOWS EDIT ELEMENTS
+function openEditMenu(selected) {
+  let taskItem = selected.closest(".task-list__item");
+  let taskMenu = taskItem.querySelector(".task-menu");
+
+  taskMenu.style.opacity = 1;
+
+  let xMark = document.createElement("i");
+  xMark.classList.add("fa-solid", "fa-xmark", "close-icon");
+  xMark.setAttribute("onclick", "showEditMenu(this)");
+
+  selected.replaceWith(xMark);
+  console.log("ON");
+}
+
+// CLOSES EDIT MENU - HIDES EDIT ELEMENTS
+function closeEditMenu(selected) {
+  let taskItem = selected.closest(".task-list__item");
+  let taskMenu = taskItem.querySelector(".task-menu");
+
+  taskMenu.style.opacity = 0;
+
+  let dotMenu = document.createElement("i");
+  dotMenu.classList.add("fa-solid", "fa-ellipsis", "dot-menu");
+  dotMenu.setAttribute("onclick", "showEditMenu(this)");
+
+  selected.replaceWith(dotMenu);
+  console.log("OFF");
 }
