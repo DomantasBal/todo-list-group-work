d18edd1a29e6b44aa5a9426ff95c42e7516376ad
irminaj <irminaj@github.com>
2023-03-30 20:44:17 +0300

    fix favourite functionality

--- favourites/favoritesIcon.js
+++ favourites/favoritesIcon.js
@@ -1,19 +1,19 @@
-let counterFavIcon = 0;
-function facIconChange(icon) {
-  counterFavIcon++;
-  counterFavIcon % 2 == 1 ? iconFilled(icon) : iconEmpty(icon);
-}
+// let counterFavIcon = 0;
+// function facIconChange(icon) {
+//   counterFavIcon++;
+//   counterFavIcon % 2 == 1 ? iconFilled(icon) : iconEmpty(icon);
+// }
 
-function iconFilled(icon) {
-  let favEd = document.createElement("i");
-  favEd.classList.add("fa-solid", "fa-heart", "favorite-icon");
-  favEd.setAttribute("onclick", "facIconChange(this)");
-  icon.replaceWith(favEd);
-}
+// function iconFilled(icon) {
+//   let favEd = document.createElement("i");
+//   favEd.classList.add("fa-solid", "fa-heart", "favorite-icon");
+//   favEd.setAttribute("onclick", "facIconChange(this)");
+//   icon.replaceWith(favEd);
+// }
 
-function iconEmpty(icon) {
-  let favEd = document.createElement("i");
-  favEd.classList.add("fa-regular", "fa-heart", "favorite-icon");
-  favEd.setAttribute("onclick", "facIconChange(this)");
-  icon.replaceWith(favEd);
-}
+// function iconEmpty(icon) {
+//   let favEd = document.createElement("i");
+//   favEd.classList.add("fa-regular", "fa-heart", "favorite-icon");
+//   favEd.setAttribute("onclick", "facIconChange(this)");
+//   icon.replaceWith(favEd);
+// }
