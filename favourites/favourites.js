931e3258690e6073525c78d57b9ce0bd14af71e0
Domantas <92823413+DomantasBal@users.noreply.github.com>
2023-03-30 19:30:55 +0300

    updated taskTemplate2 function in favorites.js with icons

--- favourites/favourites.js
+++ favourites/favourites.js
@@ -38,7 +38,8 @@ const taskTemplate2 = (todo) => {
             <div class="task-list__right">
               <span class="task-menu show">
                 <i class="fa-solid fa-pen-to-square edit-icon"></i>
-                <i class="fa-solid fa-trash delete-icon"></i>
+                <i class="fa-solid fa-trash delete-icon" onclick='deleteTask(${todo.id}, "${todo.taskName}")'></i>
+                <i class="fa-regular fa-heart favorite-icon" onclick="facIconChange(this); addToFavourites(${todo.id})"></i>
               </span>
                 <i class="dot-menu fa-solid fa-ellipsis" onclick="showEditMenu(this)"></i>
             </div>
