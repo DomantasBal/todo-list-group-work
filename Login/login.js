5d383ae689e46c5d03f116b64bab8a46c180b95d
irminaj <irminaj@github.com>
2023-03-30 19:01:48 +0300

    modify user registration and login functions and add new file for testing favourite functionality

--- Login/login.js
+++ Login/login.js
@@ -1,15 +1,22 @@
+const userName = document.getElementById("userName");
+const userPw = document.getElementById("userPw");
+
 function check() {
-  var storedName = localStorage.getItem("name");
-  var storedPw = localStorage.getItem("pw");
+  const users = JSON.parse(localStorage.getItem("users"));
+  const loggedUser = users.filter((user) => user.username === userName.value && user.password === userPw.value);
+  loggedUser.length > 0 ? localStorage.setItem("loggedUser", JSON.stringify(loggedUser)) : alert("We couldn't find such user :(");
 
-  var userName = document.getElementById("userName");
-  var userPw = document.getElementById("userPw");
-  var userRemember = document.getElementById("rememberMe");
+  // var storedName = localStorage.getItem("name");
+  // var storedPw = localStorage.getItem("pw");
 
-  if (userName.value == storedName && userPw.value == storedPw) {
-    location.href = "/index.html";
-    alert("You have successfully logged in!");
-  } else {
-    alert("Error on login, try again :(");
-  }
+  // var userName = document.getElementById("userName");
+  // var userPw = document.getElementById("userPw");
+  // var userRemember = document.getElementById("rememberMe");
+
+  // if (userName.value == storedName && userPw.value == storedPw) {
+  //   location.href = "/index.html";
+  //   alert("You have successfully logged in!");
+  // } else {
+  //   alert("Error on login, try again :(");
+  // }
 }
--- Registration/registration.js
+++ Registration/registration.js
@@ -1,15 +1,15 @@
 function store() {
-  var name = document.getElementById("name");
-  var pw = document.getElementById("pw");
+  // var name = document.getElementById("name");
+  // var pw = document.getElementById("pw");
   var lowerCaseLetters = /[a-z]/g;
   var upperCaseLetters = /[A-Z]/g;
   var numbers = /[0-9]/g;
 
-  if (name.value.length == 0) {
+  if (username.value.length == 0) {
     alert("Please fill in email!");
   } else if (pw.value.length == 0) {
     alert("Please fill in password!");
-  } else if (name.value.length == 0 && pw.value.length == 0) {
+  } else if (username.value.length == 0 && pw.value.length == 0) {
     alert("Please fill in email and password!");
   } else if (pw.value.length < 8) {
     alert("Atleast 8 symbols!");
@@ -20,9 +20,25 @@ function store() {
   } else if (!pw.value.match(lowerCaseLetters)) {
     alert("please add 1 lovercase letter!");
   } else {
-    localStorage.setItem("name", name.value);
-    localStorage.setItem("pw", pw.value);
+    // localStorage.setItem("name", name.value);
+    // localStorage.setItem("pw", pw.value);
+    saveUser();
     location.href = "../Login/login.html";
     alert("Your account has been created! :)");
   }
 }
+
+// Function to save registered user
+const username = document.getElementById("name");
+const pw = document.getElementById("pw");
+
+const saveUser = () => {
+  let users;
+  let user = { username: username.value, password: pw.value };
+  console.log(user);
+  if (!localStorage["users"]) users = [];
+  else users = JSON.parse(localStorage["users"]);
+  if (!(users instanceof Array)) users = [];
+  users.push(user);
+  localStorage.setItem("users", JSON.stringify(users));
+};
