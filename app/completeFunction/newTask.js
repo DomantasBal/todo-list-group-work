a0e4e5f6047059681fe497cb96031a26d442275a
irminaj <irminaj@github.com>
2023-04-06 21:02:50 +0300

    add function to mark completed tasks

--- app/completeFunction/newTask.js
+++ app/completeFunction/newTask.js
@@ -15,7 +15,7 @@ newTaskInput?.addEventListener("keypress", (e) => {
 // Function for adding tasks to localStorage todo list
 const saveTasks = (newTask) => {
   let todoList;
-  let task = { taskName: newTask, favourite: false };
+  let task = { taskName: newTask, favourite: false, completed: false };
   console.log(task);
   if (!localStorage["todo-list"]) todoList = [];
   else todoList = JSON.parse(localStorage["todo-list"]);
@@ -32,8 +32,7 @@ const taskTemplate = (todo) => {
   <li class="task-list__item" id=${todo.id}>
             <div class="task-list__left">
               <form action="" class="done-task-form">
-                <input type="checkbox" class="checkbox" onclick="markDone(this)" />
-                <p class="task-list__text-output" onclick="markDone(this)">${todo.taskName}</p>
+                <p class="task-list__text-output" onclick="markDone(this, ${todo.id})">${todo.taskName}</p>
               </form>
             </div>
             <div class="task-list__right">
@@ -53,8 +52,7 @@ const taskTemplateFavourite = (todo) => {
   <li class="task-list__item" id=${todo.id}>
             <div class="task-list__left">
               <form action="" class="done-task-form">
-                <input type="checkbox" class="checkbox" onclick="markDone(this)" />
-                <p class="task-list__text-output" onclick="markDone(this)">${todo.taskName}</p>
+                <p class="task-list__text-output task-name" onclick="markDone(this, ${todo.id})">${todo.taskName}</p>
               </form>
             </div>
             <div class="task-list__right">
