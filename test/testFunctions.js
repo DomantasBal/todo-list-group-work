c4183ed7c4073cdf9a72e50b62cffb14881052a8
irminaj <irminaj@github.com>
2023-04-10 15:39:27 +0300

    add unit tests to test delete, edit, mark as favourite functionalities

--- test/testFunctions.js
+++ test/testFunctions.js
@@ -1,12 +1,56 @@
 export const saveTasks = (newTask) => {
   let todoList;
-  let task = { taskName: newTask, favourite: false };
-  console.log(task);
+  let task = { taskName: newTask, favourite: false, completed: false };
   if (!localStorage["todo-list"]) todoList = [];
   else todoList = JSON.parse(localStorage["todo-list"]);
   if (!(todoList instanceof Array)) todoList = [];
-  task.id = todoList.length;
   todoList.push(task);
+  todoList.forEach((item, index) => {
+    return (item.id = index - 1 + 1);
+  });
   localStorage.setItem("todo-list", JSON.stringify(todoList));
+  console.log(task);
   return todoList;
+};
+
+export function deleteTask(list, taskId) {
+  list.splice(taskId, 1);
+  list.forEach((item, index) => {
+    item.id = index - 1 + 1;
+    return item.id;
+  });
+  return list;
+}
+
+export function editTask(todo, taskId, inputEl) {
+  let newTaskName = inputEl;
+  if (newTaskName === "") {
+    return "Task name cannot be empty!";
+  } else {
+    let todoList = todo.map((item) => {
+      if (item.id === taskId) {
+        item.taskName = newTaskName;
+      } else {
+        item.taskName = item.taskName;
+      }
+      return item;
+    });
+    console.log(todoList);
+    return todoList;
+  }
+}
+
+// Favourite function
+
+export const addToFavourites = (loggedUser, todos, id) => {
+  if (loggedUser && loggedUser.length > 0) {
+    if (todos[id].favourite === true) {
+      todos[id].favourite = false;
+    } else {
+      todos[id].favourite = true;
+    }
+    return todos;
+  } else {
+    return "You need to log in!";
+  }
 };
--- test/testFunctions.test.js
+++ test/testFunctions.test.js
@@ -1,7 +1,74 @@
-import { saveTasks } from "./testFunctions";
+import { saveTasks, deleteTask, editTask, addToFavourites } from "./testFunctions";
 
-test("creates new task and saves it to localstorage", () => {
+// Test function for creating a task
+test("creates new task", () => {
   const newTask = "Eat pizza";
   const createNewTask = saveTasks(newTask);
-  expect(createNewTask).toEqual([{ taskName: newTask, favourite: false, id: 0 }]);
+  expect(createNewTask).toEqual([{ taskName: newTask, favourite: false, completed: false, id: 0 }]);
+});
+
+// Test function for deleting the task
+test("delete the task", () => {
+  const myList = [
+    { taskName: "Buy Milk", favourite: true, completed: false, id: 0 },
+    { taskName: "Read", favourite: false, completed: true, id: 1 },
+    { taskName: "Homework", favourite: false, completed: true, id: 2 },
+  ];
+  const deleteMyTask = deleteTask(myList, 1);
+  expect(deleteMyTask).toEqual([
+    { taskName: "Buy Milk", favourite: true, completed: false, id: 0 },
+    { taskName: "Homework", favourite: false, completed: true, id: 1 },
+  ]);
+});
+
+// Test function for editing the task
+test("edit the task", () => {
+  let todoList = [
+    { taskName: "Buy Milk", favourite: true, completed: false, id: 0 },
+    { taskName: "Read", favourite: false, completed: true, id: 1 },
+    { taskName: "Homework", favourite: false, completed: true, id: 2 },
+  ];
+  const editMyTask = editTask(todoList, 1, "Watch TV");
+  expect(editMyTask).toEqual([
+    { taskName: "Buy Milk", favourite: true, completed: false, id: 0 },
+    { taskName: "Watch TV", favourite: false, completed: true, id: 1 },
+    { taskName: "Homework", favourite: false, completed: true, id: 2 },
+  ]);
+});
+
+test("return message if input value is empty", () => {
+  let todoList = [
+    { taskName: "Buy Milk", favourite: true, completed: false, id: 0 },
+    { taskName: "Read", favourite: false, completed: true, id: 1 },
+    { taskName: "Homework", favourite: false, completed: true, id: 2 },
+  ];
+  const editMyTask = editTask(todoList, 1, "");
+  expect(editMyTask).toEqual("Task name cannot be empty!");
+});
+
+// Test function for marking task as favourite
+test("mark task as favourite", () => {
+  let loggedUser = [{ userName: "Peter", password: "Hello123" }];
+  let todosList = [
+    { taskName: "Buy Milk", favourite: true, completed: false, id: 0 },
+    { taskName: "Read", favourite: false, completed: true, id: 1 },
+    { taskName: "Homework", favourite: false, completed: true, id: 2 },
+  ];
+  const markFavouriteTask = addToFavourites(loggedUser, todosList, 2);
+  expect(markFavouriteTask).toEqual([
+    { taskName: "Buy Milk", favourite: true, completed: false, id: 0 },
+    { taskName: "Read", favourite: false, completed: true, id: 1 },
+    { taskName: "Homework", favourite: true, completed: true, id: 2 },
+  ]);
+});
+
+test("return you need to log in message, if want to mark task as favourite when not logged in", () => {
+  let loggedUser = [];
+  let todosList = [
+    { taskName: "Buy Milk", favourite: true, completed: false, id: 0 },
+    { taskName: "Read", favourite: false, completed: true, id: 1 },
+    { taskName: "Homework", favourite: false, completed: true, id: 2 },
+  ];
+  const markFavouriteTask = addToFavourites(loggedUser, todosList, 2);
+  expect(markFavouriteTask).toEqual("You need to log in!");
 });
